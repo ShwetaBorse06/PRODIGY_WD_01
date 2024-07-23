6cd1acd523bbfbb0b96607678ba870c6fbead96b
ShwetaBorse06 <122162732+ShwetaBorse06@users.noreply.github.com>
2024-07-23 19:30:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#222';
-    } else {
-        navbar.style.backgroundColor = '#333';
-    }
-});
+function hover(x) {
+    x.style.color = "red";
+    x.style.backgroundColor = "#74564c";
+    x.style.textDecoration = "underline";
+    x.style.cursor = "pointer";
+}
+
+function noHover(x) {
+    x.style.color = "#955d19";
+    x.style.backgroundColor = "transparent";
+    x.style.textDecoration = "none";
+}
+
+function myHome(x) {
+    location.href = "#home"
+}
+
+function myAbout(x) {
+    location.href = '#about';
+}
+
+function myServices(x) {
+    location.href = '#services';
+}
+
+function myContact(x) {
+    location.href = "#contact";
+}
+
+function mailOver(x) {
+    x.style.textDecoration = "underline";
+}
+
+function mailOut(x) {
+    x.style.textDecoration = "none";
+    x.style.color = "000";
+}
+
+
+// responsive
+function closeNav() {
+    document.getElementById("menu").style.width = "0%";
+}
+
+function openNav() {
+    document.getElementById("menu").style.width = "100%";
+}
